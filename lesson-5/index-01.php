037bd5b87b5c019ea6d3885d1ed0d26e72c08b91
Роман Тихонов <tihonov@mediahills.ru>
2021-10-25 20:08:37 +0300

    Практическое задание. Урок 5
    Сортировка по популярности

--- lesson-5/index-01.php
+++ lesson-5/index-01.php
@@ -2,7 +2,7 @@
 
 require_once 'db.php';
 
-$result = mysqli_query( $link, 'select id, image_patch, viewed from test.images' );
+$result = mysqli_query( $link, 'select id, image_patch, viewed from test.images order by viewed' );
 
 while ( $row = mysqli_fetch_assoc($result) ) {
   echo "<a href=\"/lesson-5/get_image.php?id=$row[id]\" target=\"_blank\">
